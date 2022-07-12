328edf4f43f4ba7047f05bb099d4e4d648fd73f0
makaf <cma595@usask.ca>
2022-07-12 15:04:39 -0600

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+            'X-RapidAPI-Key': 'b34fd697c8msh577ff46a7387000p13a1f6jsnaa71752f6803'
+           
+        }
+
+        
+    });
+      
+    return data;
+}
